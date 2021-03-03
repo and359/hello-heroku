5d73c5df9bc0a761e46efe43cb441b780a9e6c9a
and359 <79313508+and359@users.noreply.github.com>
2021-03-03 23:13:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,31 @@ echo "<tr><th>Table Heading</th><th>Table Heading</th><th>Table Heading</th><th>
 echo "<tr><td>Hello world.</td><td>Hello world.</td><td>Hello world.</td><td>Hello world.</td></tr>";
 
 echo "</table>";
+  
+$servername = "us-cdbr-east-03.cleardb.com";
+$username = "b8a00bf633cf68";
+$password = "1a8113a0";
+$dbname = "heroku_69459908ed082cc";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM heroku_69459908ed082cc.`s&p500_returns`;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Ticker: " . $row["Ticker"]. " - Value Date: " . $row["ValueDate"]. " - Close Price: " . $row["ClosePx"]. " Returns: " . $row["Returns_Percent"]. " - Volume: " . $row["Volume"]. " - Uploaded: " . $row["UploadTime"]. " " . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
 
 </body>
