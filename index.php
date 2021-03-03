8e03204c806c3cd49797509ee532fd6a0324176c
and359 <79313508+and359@users.noreply.github.com>
2021-03-03 20:00:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 <?php
 echo "FINALLY!!! AUNTY IS STAYING!!!!";
   
-echo “<table>”;
+echo "<table>";
 
-echo “<tr><th>Table Heading</th><th>Table Heading</th><th>Table Heading</th><th>Table Heading</th></tr>”;
+echo "<tr><th>Table Heading</th><th>Table Heading</th><th>Table Heading</th><th>Table Heading</th></tr>";
 
 echo "<tr><td>Hello world.</td><td>Hello world.</td><td>Hello world.</td><td>Hello world.</td></tr>";
 
-echo “</table”>;
+echo "</table>";
 ?>
 
 </body>
