3585ddd8e7a13c2cecacdd4d7a750be7bf275491
and359 <79313508+and359@users.noreply.github.com>
2021-03-04 10:51:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,36 +2,7 @@
 <html>
 <body>
 
-<h1>My first PHP page 1 2 3</h1>
-	
-<?php
-//echo "FINALLY!!! AUNTY IS STAYING!!!!";
-  
-$servername = "us-cdbr-east-03.cleardb.com";
-$username = "b8a00bf633cf68";
-$password = "1a8113a0";
-$dbname = "heroku_69459908ed082cc";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM heroku_69459908ed082cc.`s&p500_returns`;";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Ticker: " . $row["Ticker"]. " - Value Date: " . $row["ValueDate"]. " - Close Price: " . $row["ClosePx"]. " Returns: " . $row["Returns_Percent"]. " - Volume: " . $row["Volume"]. " - Uploaded: " . $row["UploadTime"]. " " . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
-?>
+<h1>S&P500 Performance (PHP)</h1>
 	
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <script
@@ -2187,5 +2158,34 @@ Highcharts.chart('container1', {
     }]
 
 });</script>
+	
+<?php
+//echo "FINALLY!!! AUNTY IS STAYING!!!!";
+  
+$servername = "us-cdbr-east-03.cleardb.com";
+$username = "b8a00bf633cf68";
+$password = "1a8113a0";
+$dbname = "heroku_69459908ed082cc";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM heroku_69459908ed082cc.`s&p500_returns`;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Ticker: " . $row["Ticker"]. " - Value Date: " . $row["ValueDate"]. " - Close Price: " . $row["ClosePx"]. " Returns: " . $row["Returns_Percent"]. " - Volume: " . $row["Volume"]. " - Uploaded: " . $row["UploadTime"]. " " . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
 </body>
 </html>
