10f7de903a539eb34154fbcca54925f605a9c96c
and359 <79313508+and359@users.noreply.github.com>
2021-03-05 16:47:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2286,11 +2286,12 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-	echo "<table border=1>";
+	echo "<table border=1><tr><th>#</th><th>Ticker</th><th>Desc</th><th>Close Price</th><th>Returns</th><th>Sector</th><th>Industry</th><th>Volume</th></tr>";
+	
     while($row = $result->fetch_assoc()) {
         //echo ++$row_num . ") Ticker: " . $row["Ticker"]. " - Value Date: " . $row["ValueDate"]. " - Close Price: " . $row["ClosePx"]. " Returns: " . $row["Returns_Percent"]. " - Volume: " . $row["Volume"]. " - Uploaded: " . $row["UploadTime"]. " " . "<br>";
 	
-	echo "<tr><td>" . ++$row_num . ")</td><td> Ticker: " . $row["Ticker"]. "</td><td>Desc: " . $row["Desc"]. "</td><td>Close Price: " . $row["LastPx"]. "</td><td>Returns: " . $row["RtnPercent"]. "</td><td>Sector: " . $row["Sector"]. "</td><td>Industry: " . $row["Industry"]. "</td><td>Volume: " . $row["Volume"]. "</td></tr>";
+	echo "<tr><td>" . ++$row_num . ")</td><td>" . $row["Ticker"]. "</td><td>" . $row["Desc"]. "</td><td>" . $row["LastPx"]. "</td><td>" . $row["RtnPercent"]. "</td><td>" . $row["Sector"]. "</td><td>" . $row["Industry"]. "</td><td>" . $row["Volume"]. "</td></tr>";
 	
     }	echo "</table>";
 } else {
