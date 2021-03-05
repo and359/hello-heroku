198f20b4a12c046b195fecdf55daecf90bd8db10
and359 <79313508+and359@users.noreply.github.com>
2021-03-05 16:43:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2286,12 +2286,13 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
+	echo "<table border=1>";
     while($row = $result->fetch_assoc()) {
         //echo ++$row_num . ") Ticker: " . $row["Ticker"]. " - Value Date: " . $row["ValueDate"]. " - Close Price: " . $row["ClosePx"]. " Returns: " . $row["Returns_Percent"]. " - Volume: " . $row["Volume"]. " - Uploaded: " . $row["UploadTime"]. " " . "<br>";
-	echo "<table border=1>";
+	
 	echo "<tr><td>" . ++$row_num . ")</td><td> Ticker: " . $row["Ticker"]. "</td><td>Desc: " . $row["Desc"]. "</td><td>Close Price: " . $row["LastPx"]. "</td><td>Returns: " . $row["RtnPercent"]. "</td><td>Sector: " . $row["Sector"]. "</td><td>Industry: " . $row["Industry"]. "</td><td>Volume: " . $row["Volume"]. "</td></tr>";
-	echo "</table>";
-    }
+	
+    }	echo "</table>";
 } else {
     echo "0 results";
 }
