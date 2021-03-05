1bd501991f2d01ea1aa84b6765866c28cd500231
and359 <79313508+and359@users.noreply.github.com>
2021-03-05 16:16:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2284,7 +2284,7 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         //echo ++$row_num . ") Ticker: " . $row["Ticker"]. " - Value Date: " . $row["ValueDate"]. " - Close Price: " . $row["ClosePx"]. " Returns: " . $row["Returns_Percent"]. " - Volume: " . $row["Volume"]. " - Uploaded: " . $row["UploadTime"]. " " . "<br>";
-	echo "<table border="1"><tr>" . ++$row_num . ") Ticker: " . $row["Ticker"]. " - Desc: " . $row["Desc"]. " - Close Price: " . $row["LastPx"]. " Returns: " . $row["RtnPercent"]. " - Sector: " . $row["Sector"]. " - Industry: " . $row["Industry"]. " - Volume: " . $row["Volume"]. "</td></table>" . "<br>";
+	echo "<table border=1><tr>" . ++$row_num . ") Ticker: " . $row["Ticker"]. " - Desc: " . $row["Desc"]. " - Close Price: " . $row["LastPx"]. " Returns: " . $row["RtnPercent"]. " - Sector: " . $row["Sector"]. " - Industry: " . $row["Industry"]. " - Volume: " . $row["Volume"]. "</td></table>" . "<br>";
     }
 } else {
     echo "0 results";
