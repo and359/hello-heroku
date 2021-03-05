f08d82c1c16e991a9a19fe59a08d99bfda9d09af
and359 <79313508+and359@users.noreply.github.com>
2021-03-05 13:11:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <!DOCTYPE html>
 <html>
+	
+	<head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+		<title>Accelerometer data</title>
+
+		<style type="text/css">			
+			body{
+				font-family: Arial;
+			    margin: 80px 100px 10px 100px;
+			    padding: 0;
+			    color: white;
+			    text-align: center;
+			    background: #555652;
+			}
+
+			.container {
+				color: #E8E9EB;
+				background: #222;
+				border: #555652 1px solid;
+				padding: 10px;
+			}
+		</style>
+
+	</head>
+
 <body>
 
 <h1>S&P500 Performance (PHP)</h1>
