340e74f22f81a0dd051aee89717ec51352e98b2e
and359 <79313508+and359@users.noreply.github.com>
2021-03-05 22:32:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
-		<title>Accelerometer data</title>
+		<title>S&P500</title>
 
 		<!-- 
 			<style type="text/css">			
