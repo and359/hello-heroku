50f749a27e4f7a857383dd52bec49cea9b7aa948
and359 <79313508+and359@users.noreply.github.com>
2021-03-05 13:08:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2161,9 +2161,7 @@ Highcharts.chart('container1', {
 
 });</script>
 
-	/*		*/
-	/*line chart	*/
-	/*		*/
+	***line chart 
 <?php
 	/* Database connection settings */
 	$host = 'us-cdbr-east-03.cleardb.com';
@@ -2189,12 +2187,44 @@ Highcharts.chart('container1', {
 	$data1 = trim($data1,",");
 	$data2 = trim($data2,",");
 ?>
-	
+	<div class="container">	
+	    <h1>USE CHART.JS WITH MYSQL DATASETS</h1>       
+			<canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
 
-	/*			*/
-	/*end line chart	*/
-	/*			*/
+			<script>
+				var ctx = document.getElementById("chart").getContext('2d');
+    			var myChart = new Chart(ctx, {
+        		type: 'line',
+		        data: {
+		            labels: [1,2,3,4,5,6,7,8,9],
+		            datasets: 
+		            [{
+		                label: 'Data 1',
+		                data: [<?php echo $data1; ?>],
+		                backgroundColor: 'transparent',
+		                borderColor:'rgba(255,99,132)',
+		                borderWidth: 3
+		            },
+
+		            {
+		            	label: 'Data 2',
+		                data: [<?php echo $data2; ?>],
+		                backgroundColor: 'transparent',
+		                borderColor:'rgba(0,255,255)',
+		                borderWidth: 3	
+		            }]
+		        },
+		     
+		        options: {
+		            scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},
+		            tooltips:{mode: 'index'},
+		            legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}}
+		        }
+		    });
+			</script>
+	    </div>
 	
+	***line chart end
 	
 	
 	
