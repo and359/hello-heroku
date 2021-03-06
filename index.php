c48d116ea012f0e1b1d309f55e38bf33ef3cf7d0
and359 <79313508+and359@users.noreply.github.com>
2021-03-07 00:33:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body>
 
 	
-<h1>S&P500 Performance (PHP) updated 34</h1>
+<h1>S&P500 Performance (PHP) updated 35</h1>
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -238,7 +238,7 @@ Highcharts.chart('container1', {
 		            scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},
 		            tooltips:{mode: 'index'},
 		            legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}},
-				title: {text: '2017 Tour de France Stage 8: Dole - Station des Rousses'},
+				title: {text: '2017 Tour de France Stage 8: Dole - Station des Rousses'}
 		        }
 				
     }
