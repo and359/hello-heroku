e31b52acf14a185dee6b7924b85e9b69c5065ecf
and359 <79313508+and359@users.noreply.github.com>
2021-03-07 01:32:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
+<head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+      <script async src="//jsfiddle.net/beaver71/3qkcLk6v/embed/"></script>
+		<title>Accelerometer data</title>
 
-<script async src="//jsfiddle.net/beaver71/3qkcLk6v/embed/"></script>
-   
-   
+		<style type="text/css">			
+			body{
+				font-family: Arial;
+			    margin: 80px 100px 10px 100px;
+			    padding: 0;
+			    color: white;
+			    text-align: center;
+			    background: #555652;
+			}
+
+			.container {
+				color: #E8E9EB;
+				background: #222;
+				border: #555652 1px solid;
+				padding: 10px;
+			}
+		</style>
+
+	</head>
+
+
+<body>	   
+	    <div class="container">	
+	    <h1>USE CHART.JS WITH MYSQL DATASETS</h1>       
+			<canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
 <script>
 
    
@@ -55,3 +82,5 @@ var chart = new Chart(ctx, {
 
 
       </script>
+          </div>
+</body>
