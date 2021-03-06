ede8789e15b2f8d2c5b4cc41873589dde6f8a93d
and359 <79313508+and359@users.noreply.github.com>
2021-03-06 17:54:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body>
 
 	
-<h1>S&P500 Performance (PHP) updated4</h1>
+<h1>S&P500 Performance (PHP) updated5</h1>
 	
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <script
@@ -44,7 +44,7 @@
 </div>
 	
 	
-
+<script type="text/javascript">
 // Now create the chart
 Highcharts.chart('container1', {
 
