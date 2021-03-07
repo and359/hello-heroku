8a09f7c7efb810de4758d1bda34d10d4eed40d66
and359 <79313508+and359@users.noreply.github.com>
2021-03-07 17:01:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	
 <body>
 
-<h1>S&P500 Performance (PHP) updated 34</h1>
+<h1>S&P500 Performance (PHP) updated 35</h1>
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -255,7 +255,7 @@ Highcharts.chart('container1', {
 		      borderColor: 'green',
 		      borderWidth: 1,
 		      label: {
-			 enabled: true,
+			 //enabled: true,
 			 //position: "center",
 			 content: amount[index]
 		      }
