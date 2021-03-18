e384efffd8b61009f70da681c30fe917912f8033
and359 <79313508+and359@users.noreply.github.com>
2021-03-18 12:00:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Administrator 1</title>
+	<title>Trading Analytics</title>
 	<!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
@@ -20,10 +20,10 @@
 </head>
 <style type="text/css">
 .bg{
-	background-image: linear-gradient(to top left,#8a4592,#35b0a9);
+	background-image: linear-gradient(to top left,red,yellow);
 }
 nav{
-	background-image: linear-gradient(to top right,#35b0a9,#8a4592); 
+	background-image: linear-gradient(to top right,black,black); 
 	padding-left: 300px;
 }
 .content{
@@ -48,7 +48,7 @@ nav{
 	<div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo center">Administrator</a>
+			<a href="#" class="brand-logo center">Administrator 1</a>
 			<a href="" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>		
 		</div>
 	</nav>
