585265576426e7a7d9c86f0a219eea09c7b17417
and359 <79313508+and359@users.noreply.github.com>
2021-03-18 20:52:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 	</head>
 
 <body>
-<h1>S&P500 Performance (PHP) 45</h1>
+<h1>S&P500 Performance (PHP) 46</h1>
 
 	
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -345,7 +345,7 @@ Highcharts.chart('container1', {
                 + ' date, ' + this.y ;
             },//headerFormat: 'Date: {point.x}<br>',
         //pointFormat: '{point.y}',
-        //shared: true
+        shared: true
     },
 
     legend: {
