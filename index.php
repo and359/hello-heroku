4437292638496cb27745fa1e030bd3bd5378c180
and359 <79313508+and359@users.noreply.github.com>
2021-03-18 16:11:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 	</head>
 
 <body>
-<h1>S&P500 Performance (PHP) 22</h1>
+<h1>S&P500 Performance (PHP) 23</h1>
 
 	
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -118,7 +118,7 @@
 		
 			
 			return [
-			new moment(marketing[index1], "MM-DD-YYYY"),amount[index1]
+			moment(marketing[index1], "DD/MM/YYYY"), amount[index1]
 			];
 		
 		});
