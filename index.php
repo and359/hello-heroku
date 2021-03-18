ee4ac28f32b3073b5fe3f503db182ff62a08e49c
and359 <79313508+and359@users.noreply.github.com>
2021-03-18 16:01:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 	</head>
 
 <body>
-<h1>S&P500 Performance (PHP) 19</h1>
+<h1>S&P500 Performance (PHP) 20</h1>
 
 	
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
@@ -117,7 +117,7 @@
 		
 			
 			return [
-			marketing[index1],amount[index1]
+			new Date(marketing[index1]),amount[index1]
 			];
 		
 		});
