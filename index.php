57543aeaed4cf3b1b3b1d0d8a98cd655a0d5c41f
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 02:19:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
-<html>
-<head>
-	
-	
-	
-	
-	
-      
-</head>
-<body>
-	8<br>
-	
-	
-	
-	<table class="table table-bordered table-striped">
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Collapse test</title>
+        <!--<link href="css/bootstrap.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap-collapse.js"></script>-->
+	   <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet">
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+    <table class="table table-bordered table-striped">
         <tr>
             <td>
               <button type="button" class="btn" data-toggle="collapse" data-target="#collapseme">
@@ -21,14 +20,11 @@
               </button>
             </td>
         </tr>
-        <tr id="collapseme" class="collapse out"><td><div>Should be collapsed</div></td></tr>
+        <tr class="collapse out" id="collapseme"><td><div>Should be collapsed</div></td></tr>
     </table>
-	
-	<!--<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css"></script>
-	<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
-	<script type="text/javascript" src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>-->
-	<script src="http://code.jquery.com/jquery.js"></script>
-	<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
-	 		
 </body>
 </html>
+	
+	
+	
+	 		
