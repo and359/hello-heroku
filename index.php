6a11266d86e238acadf59e17f7c04e8f84a1af31
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 01:54:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
       
 </head>
 <body>
-	2<br>
+	3<br>
 	
 	<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css"></script>
-	<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
-	<script type="text/javascript" src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+	<!--<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+	<script type="text/javascript" src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>-->
 	
 	<table class="table table-bordered table-striped">
         <tr>
