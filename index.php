bb45cb5879ece442305a23e459d3be7f7e4250d2
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 02:34:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,21 +3,51 @@
     <head>
         <meta charset="utf-8">
         <title>Collapse test</title>
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap-collapse.js"></script>
+        <!--<link href="css/bootstrap.css" rel="stylesheet">-->
+        <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <!--<script src="js/bootstrap-collapse.js"></script>-->
     </head>
     <body>
 
-    <table class="table table-bordered table-striped">
-        <tr>
-            <td>
-              <button type="button" class="btn" data-toggle="collapse" data-target="#collapseme">
-                Click to expand
-              </button>
-            </td>
-        </tr>
-        <tr><td><div class="collapse out" id="collapseme">Should be collapsed</div></td></tr>
-    </table>
+        
+        <script>
+            (function() {
+	$('#carsTable .toggle').on('click', function() {
+  	$('#carsTable .hideableRow').toggleClass('hiddenRow');
+  })
+})()
+        </script>
+        
+   <table class="table table-sm table-hover">
+<thead class="thead-inverse">
+
+<thead>
+    <tr>
+        <th colspan="6"></th>
+        <th colspan="3">Current Month</th>
+        <th colspan="3">Year-to-Date</th>
+    </tr>
+</thead>
+<tbody>
+    <tr data-toggle="collapse" data-target="#cars" class="accordion-toggle">
+        <th colspan="6">Cars</th>
+        <td colspan="3">456 mi</td>
+        <td colspan="3">700 mi</td>
+    </tr>
+    <tr class="hiddenRow"><div class="accordian-body collapse" id="cars">
+        <td colspan="1"></td>
+        <td colspan="5">Toyota</td>
+        <td colspan="3">534 mi</td>
+        <td colspan="3">800 mi</td>
+    </tr>
+    <tr>
+        <th colspan="1"></th>
+        <th colspan="5">Honda</th>
+        <td colspan="3">600 mi</td>
+        <td colspan="3">770 mi</td>
+    </tr>
+    </div>
+</tbody>
+</table>
 </body>
 </html>
