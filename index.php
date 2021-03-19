4fb74200157c39184bf1729072f33bb17208725d
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 01:34:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,24 @@
 <head>
 	
 	
-	
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css"></script>
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>	
 	
       
 </head>
 <body>
 	
+	<div class="navbar-fixed">
+	<nav>
+		<div class="nav-wrapper">
+			<a href="#" class="brand-logo center">Trading Results: 1</a>
+			<a href="" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>		
+		</div>
+	</nav>
+	</div>
 
-	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css"></script>
-	<script src="http://code.jquery.com/jquery.js"></script>
-	<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>	
+	
 	
 	
 	<table class="table table-bordered table-striped">
