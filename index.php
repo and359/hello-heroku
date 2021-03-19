148a74361658247bac5f6fa8fb162222bca7f66d
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 01:45:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,17 @@
 	<!--<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css"></script>
 	<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
 	<script type="text/javascript" src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>-->
-	<script async src="//jsfiddle.net/KnuU6/21/embed/"></script>
+	
 	
       
 </head>
 <body>
+	<script async src="//jsfiddle.net/KnuU6/21/embed/"></script>
 	
 	<div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo center">Trading Results: </a>	
+			<a href="#" class="brand-logo center">Trading Results: 1</a>	
 		</div>
 	</nav>
 	</div>
