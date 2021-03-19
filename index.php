9ad630b8becd3f2c46566a03327bdaa64114a94b
and359 <79313508+and359@users.noreply.github.com>
2021-03-19 11:20:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -20,24 +20,10 @@
 	
 </head>
 <style type="text/css">
-.bg{
-	background-image: linear-gradient(to top left,black,blue);
-}
-nav{
-	background-image: linear-gradient(to top right,red,yellow); 
-	padding-left: 300px;
-}
-.content{
-	padding-left: 300px;
-	height:100%;
-}
-.card-bg{
-	background: rgba(0,0,0,0);
-}
-@media only screen and (max-width: 992px){
-	.content,nav{
-		padding-left: 0;
-	}
+#container {
+    max-width: 800px;
+    height: 400px;
+    margin: 1em auto;
 }
 </style>
 <script type="text/javascript">
@@ -45,6 +31,7 @@ nav{
 		$('.sidenav').sidenav();
 	});
 </script>
+	
 <body>
 	<script
   src="https://code.jquery.com/jquery-3.5.1.min.js"
@@ -58,373 +45,75 @@ nav{
 <script src="https://code.highcharts.com/modules/accessibility.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
 	
-	<div class="navbar-fixed">
-	<nav>
-		<div class="nav-wrapper">
-			<a href="#" class="brand-logo center">Administrator 1</a>
-			<a href="" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>		
-		</div>
-	</nav>
-	</div>
-	<ul class="sidenav sidenav-fixed bg" id="slide-out">
-		<li>
-			<div class="user-view">
-				<div class="background">
-					<img src="cd.png" width="100%">
-				</div>
-				<a href="#"><img src="pp.jpg" class="circle"></a>
-				<a href="#" class="white-text name">Maya Circle</a>
-				<a href="#" class="white-text email">MayaCircle@email.com</a>
-			</div>	
-		</li>
-		<li><a href="" class="white-text"><i class="material-icons">home</i>Dashboard</a></li>
-		<li><a href="" class="white-text"><i class="material-icons">mail</i>Data Member</a></li>
-	</ul>
-	<div class="content bg">
-		<div class="container">
-		<div class="row">
-			<div class="col s12">
-				<h1 class="white-text">DashBoard</h1>
-			</div>
-			<div class="col s12 m6 l3">
-				<div class="card card-bg white-text">
-					<div class="card-content center">
-						<p>Revenue</p>
-						<h5>$12,476.00</h5>
-						<i class="material-icons small green-text">keyboard_arrow_up</i><br>
-						<b class="green-text">%12</b>
-					</div>
-				</div>
-			</div>
-			<div class="col s12 m6 l3">
-				<div class="card card-bg white-text">
-					<div class="card-content center">
-						<p>Click</p>
-						<h5>2400</h5>
-						<i class="material-icons small red-text">keyboard_arrow_down</i><br>
-						<b class="red-text">%10</b>
-					</div>
-				</div>
-			</div>
-			<div class="col s12 l3 m6">
-				<div class="card card-bg white-text">
-					<div class="card-content center">
-						<p>Users</p>
-						<h5>5000,00</h5>
-						<i class="material-icons small green-text">keyboard_arrow_up</i><br>
-						<b class="green-text">%7</b>
-					</div>
-				</div>
-			</div>
-			<div class="col s12 l3 m6">
-				<div class="card card-bg white-text">
-					<div class="card-content center">
-						<p>Conversion Rate</p>
-						<h5>0,80%</h5>
-						<i class="material-icons small green-text">keyboard_arrow_up</i><br>
-						<b class="green-text">%25</b>
-					</div>
-				</div>
-			</div>
-			<div class="col l12 m6 s12">
-				<div class="card card-bg">
-					<div class="card-content">
-						<canvas id="myChart"></canvas>
-					</div>
-				</div>
-			</div>
-			<div class="col l12 m6 s12">
-				<div class="card card-bg">
-					<div class="card-content">
-						<canvas id="myChart2"></canvas>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	</div>
-	
-	<!--get data from mysql-->
-	<?php
-	/* Database connection settings */
-	$host = 'us-cdbr-east-03.cleardb.com';
-	$user = 'b8a00bf633cf68';
-	$pass = '1a8113a0';
-	$db = 'heroku_69459908ed082cc';
-	$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+	<div id='container'></div>
+<button id='save_btn'>Save Chart</button>
 
-	$data1 = '';
-	$data2 = '';
-	$date = '';
-	$data3 = '';
-	$data4 = '';
-	$data5 = '';
-	$data6 = '';
-	
+<script>EXPORT_WIDTH = 1000;
 
-	//query to get data from the table
-	$sql = "SELECT * FROM `backtest`;";
-    	$result = mysqli_query($mysqli, $sql);
+function save_chart(chart) {
+    var render_width = EXPORT_WIDTH;
+    var render_height = render_width * chart.chartHeight / chart.chartWidth
 
-	//loop through the returned data
-	while ($row = mysqli_fetch_array($result)) {
-
-		$data1 = $data1 . '"'. $row['Price'].'",';
-		$date = $date . '"'. $row['PriceDate'] .'",';
-		$data6 = $data6 . '"'. $row['UnixTime'].'",';
-	}
-
-	$data1 = trim($data1,",");
-	//$data2 = trim($data2,",");
-	$date = trim($date,",");
-	$data6 = trim($data6,",");
-	
-	$sql = "select Ticker from `heroku_69459908ed082cc`.`backtest` order by Ticker desc limit 1;";
-    	$result = mysqli_query($mysqli, $sql);
-	$row = mysqli_fetch_array($result);
-	$data2 = $data2 . $row['Ticker'];
-	
-	$sql = "select * from `heroku_69459908ed082cc`.`buy_sell`;";
-    	$result = mysqli_query($mysqli, $sql);
-	while ($row = mysqli_fetch_array($result)) {
-
-		$data3 = $data3 . '"'. $row['TradeDate'].'",';
-		$data4 = $data4 . '"'. $row['Remarks'].'",';
-		$data5 = $data5 . '"'. $row['BuySell'].'",';
-		$data7 = $data7 . '"'. $row['UnixTime'].'",';
-		$data8 = $data8 . '"'. $row['Price'].'",';
-		
-	}
-
-	$data3 = trim($data3,",");
-	$data4 = trim($data4,",");
-	$data5 = trim($data5,",");
-	$data7 = trim($data7,",");
-	$data8 = trim($data8,",");
-	
-	
-	?>
-	<!--end of mysql-->
-	
-	<div class="container">
-	<div class="row">
-		<div class="col-sm-6 col-md-6">
-			<div id="container1"></div>
-		</div>
-		<div class="col-sm-6 col-md-6"></div>
-		<div class="col-sm-6 col-md-6"></div>
-		<div class="col-sm-6 col-md-6"></div>
-	</div>
-	</div>
-
-	
-	<!--<div class="container">-->	
-			
-		<!--<h1><?php echo $data2; ?> Share Price</h1>-->	   
-		<!--<canvas id="ctx" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>-->
-	
-	<script type="text/javascript">
-		var chr=document.getElementById("myChart").getContext("2d");
-		var chr2=document.getElementById("myChart2").getContext("2d");
-		
-//chart 1		
-		var marketing = [<?php echo $data6; ?>];
-		var amount = [<?php echo $data1; ?>];
-		var marketing3 = [<?php echo $data7; ?>];
-		var amount4 = [<?php echo $data4; ?>];
-		var px4 = [<?php echo $data8; ?>];
-		var B_S = [<?php echo $data5; ?>];
-		var txt = "";
-		
-		var test3 = marketing.map(function(date1, index1) {
-		
-			
-			return [
-			Number(marketing[index1]), Number(amount[index1])
-			];
-		
-		});
-		
-		//annotations	
-		var test4 = marketing3.map(function(date2, px2) {
-		
-			if (B_S[px2]=='Buy'){
-			return {
-			//type: 'line', borderColor: 'green', id: 'vline' + index2, mode: 'vertical', scaleID: 'x-axis-0', value: date2, borderWidth: 1, label: {enabled: true, position: "bottom", content: amount4[index2]}}
-			labelOptions: {backgroundColor: 'rgba(255,255,255,0.5)',verticalAlign: 'top',y: 15},labels: [{point: {xAxis: 0,yAxis: 0,x: date2,y: px4[px2]},text: amount4[px2]}]}
-			} else {
-			return{
-			//type: 'line', borderColor: 'red', id: 'vline' + index2, mode: 'vertical', scaleID: 'x-axis-0', value: date2, borderWidth: 1, label: {enabled: true, position: "top", content: amount4[index2]}}
-			labels: [{point: {xAxis: 0,yAxis: 0,x: date2,y: px4[px2]},x: -30,text: amount4[px2]}]}
-			};
-		
-		});		
-		</script>
-	
-	
-<script type="text/javascript">
-// Data generated from http://www.bikeforums.net/professional-cycling-fans/1113087-2017-tour-de-france-gpx-tcx-files.html
-	
-	
-	
-	var elevationData = test3;
-	
-	
-
-// Now create the chart
-Highcharts.chart(chr, {
-
-    chart: {
-        type: 'area',
-        zoomType: 'x',
-        panning: true,
-        panKey: 'shift',
-        scrollablePlotArea: {
-            minWidth: 600
+    // Get the cart's SVG code
+    var svg = chart.getSVG({
+        exporting: {
+            sourceWidth: chart.chartWidth,
+            sourceHeight: chart.chartHeight
         }
-    },
+    });
 
-    caption: {
-        text: 'This chart uses the Highcharts Annotations feature to place labels at various points of interest. The labels are responsive and will be hidden to avoid overlap on small screens.'
-    },
+    // Create a canvas
+    var canvas = document.createElement('canvas');
+    canvas.height = render_height;
+    canvas.width = render_width;
+    document.body.appendChild(canvas);
 
-    title: {
-        text: '2017 Tour de France Stage 8: Dole - Station des Rousses'
-    },
+    // Create an image and draw the SVG onto the canvas
+    var image = new Image;
+    image.onload = function() {
+        canvas.getContext('2d').drawImage(this, 0, 0, render_width, render_height);
+    };
+    image.src = 'data:image/svg+xml;base64,' + window.btoa(svg);
+}
 
-    accessibility: {
-        description: 'Image description: An annotated line graph illustrates the 8th stage of the 2017 Tour de France cycling race from the start point in Dole to the finish line at Station des Rousses. Altitude is plotted on the Y-axis at increments of 500m and distance is plotted on the X-axis in increments of 25 kilometers. The line graph is interactive, and the user can trace the altitude level at every 100-meter point along the stage. The graph is shaded below the data line to visualize the mountainous altitudes encountered on the 187.5-kilometre stage. The three largest climbs are highlighted at Col de la Joux, Côte de Viry and the final 11.7-kilometer, 6.4% gradient climb to Montée de la Combe de Laisia Les Molunes which peaks at 1200 meters above sea level. The stage passes through the villages of Arbois, Montrond, Bonlieu, Chassal and Saint-Claude along the route.'
-    },
 
-    credits: {
-        enabled: false
-    },
-	
-	
-    annotations: test4
-	/*[
-	    {labelOptions: {
-            backgroundColor: 'rgba(255,255,255,0.5)',
-            verticalAlign: 'top',
-            y: 15
-        },
-        labels: [{
-            point: {
-                xAxis: 0,
-                yAxis: 0,
-                x: 1583798400000,
-                y: 57.970001
-            },
-            text: 'Arbois'
-        }]}, 
-	{labels: [{
-            point: {
-                xAxis: 0,
-                yAxis: 0,
-                x: 1613692800000,
-                y: 227.270004
-            },
-            x: -30,
-            text: 'Col de la Joux'
-        }]}
-		 ]*/,	
+$(function() {
 
-    xAxis: {
-	    labels: {
-      format: "{value:%b %e}"
-    },
-    //tickInterval: 604800000,
-    type: "datetime"//,
-    //min: 1569888000000
-	    /*,
-      labels: {
-        formatter: function() {
-          return Highcharts.dateFormat('%b/%e/%Y', this.value);
-        }
-      }*/
-        /*labels: {
-            format: '{value}'
-        },
-        minRange: 5,
-        title: {
-            text: 'Distance'
-        },
-        accessibility: {
-            rangeDescription: 'Range: 0 to 187.8km.'
-        }*/
-    },
+    $('#container').highcharts({
 
-    yAxis: {
-        startOnTick: true,
-        endOnTick: false,
-        maxPadding: 0.35,
-        title: {
-            text: null
-        },
-        labels: {
-            format: '{value}'
-        }
-    },
-
-    tooltip: {
-        formatter: function() {
-                return  '<b>' + this.series.name +'</b><br/>' +
-                    Highcharts.dateFormat('%e - %b - %Y',
-                                          new Date(this.x))
-                + ' date, ' + this.y ;
-            }//,headerFormat: 'Date: {point.x}<br>',
-        //pointFormat: '{point.y}',
-        //shared: true
-    },
-
-    legend: {
-        enabled: false
-    },
-
-    series: [{
-        accessibility: {
-            keyboardNavigation: {
-                enabled: false
-            }
-        },
-        data: elevationData,
-        lineColor: Highcharts.getOptions().colors[1],
-        color: Highcharts.getOptions().colors[2],
-        fillOpacity: 0.5,
-        name: 'Elevation',
-        marker: {
+        exporting: {
             enabled: false
         },
-        threshold: null
-    }]
 
-});		
-//end of chart 1		
-		var myChart2=new Chart(chr2,{
-			type:'line',
-			data:{
-				labels:['Monday','Tuesday','Wednesday','Thursday','fiday'],
-				datasets:[{
-					label:'Data Users',
-					data:[100,512,150,120,190],
-					backgroundColor:'rgba(0,0,0,0)',
-					borderColor:'#fff',
-					borderWidth:1,
-				}]
-			},
-			options:{
-				legend:{
-					labels:{
-						fontColor:'#fff',
-					}
-				}
-			}
-		});
-	</script>
-	<!--</div>-->
+        title: {
+            text: 'Client-Side Download Example'
+        },
 
+        subtitle: {
+            text: 'Click "Save Chart" to copy chart into canvas'
+        },
 
+        chart: {
+            type: 'area'
+        },
 
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+
+        series: [{
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        }]
+
+    });
+    
+    $('#save_btn').click(function() {
+        save_chart($('#container').highcharts());
+    });
+
+});
+</script>
 
 
 
