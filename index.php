d3858e806562988e3f4274251d65b3d49e2c4d58
and359 <79313508+and359@users.noreply.github.com>
2021-03-19 09:53:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <!--Highchart-->
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+      <!--End Of Highchart-->
 	
       <!--chart.js annotation plugin-->
       <script>
@@ -45,6 +48,22 @@ nav{
 	});
 </script>
 <body>
+	
+	<!--Highchart-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/annotations.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+	<!--End Of Highchart-->
+	
 	<div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
@@ -146,6 +165,8 @@ nav{
 	$data3 = '';
 	$data4 = '';
 	$data5 = '';
+	$data6 = '';
+	
 
 	//query to get data from the table
 	$sql = "SELECT * FROM `backtest`;";
@@ -156,11 +177,13 @@ nav{
 
 		$data1 = $data1 . '"'. $row['Price'].'",';
 		$date = $date . '"'. $row['PriceDate'] .'",';
+		$data6 = $data6 . '"'. $row['UnixTime'].'",';
 	}
 
 	$data1 = trim($data1,",");
 	//$data2 = trim($data2,",");
 	$date = trim($date,",");
+	$data6 = trim($data6,",");
 	
 	$sql = "select Ticker from `heroku_69459908ed082cc`.`backtest` order by Ticker desc limit 1;";
     	$result = mysqli_query($mysqli, $sql);
@@ -174,11 +197,17 @@ nav{
 		$data3 = $data3 . '"'. $row['TradeDate'].'",';
 		$data4 = $data4 . '"'. $row['Remarks'].'",';
 		$data5 = $data5 . '"'. $row['BuySell'].'",';
+		$data7 = $data7 . '"'. $row['UnixTime'].'",';
+		$data8 = $data8 . '"'. $row['Price'].'",';
+		
 	}
 
 	$data3 = trim($data3,",");
 	$data4 = trim($data4,",");
 	$data5 = trim($data5,",");
+	$data7 = trim($data7,",");
+	$data8 = trim($data8,",");
+	
 	
 	?>
 	<!--end of mysql-->
@@ -193,22 +222,170 @@ nav{
 		var chr2=document.getElementById("myChart2").getContext("2d");
 		
 //chart 1		
-		var marketing = [<?php echo $data3; ?>];
-		var amount = [<?php echo $data4; ?>];
+		var marketing = [<?php echo $data6; ?>];
+		var amount = [<?php echo $data1; ?>];
+		var marketing3 = [<?php echo $data7; ?>];
+		var amount4 = [<?php echo $data4; ?>];
+		var px4 = [<?php echo $data8; ?>];
 		var B_S = [<?php echo $data5; ?>];
 		var txt = "";
 		
 		var test3 = marketing.map(function(date1, index1) {
 		
-			if (B_S[index1]=='Buy'){
+			
+			return [
+			Number(marketing[index1]), Number(amount[index1])
+			];
+		
+		});
+		
+		//annotations	
+		var test4 = marketing3.map(function(date2, px2) {
+		
+			if (B_S[px2]=='Buy'){
 			return {
-			type: 'line', borderColor: 'green', id: 'vline' + index1, mode: 'vertical', scaleID: 'x-axis-0', value: date1, borderWidth: 1, label: {enabled: true, position: "bottom", content: amount[index1]}}
+			//type: 'line', borderColor: 'green', id: 'vline' + index2, mode: 'vertical', scaleID: 'x-axis-0', value: date2, borderWidth: 1, label: {enabled: true, position: "bottom", content: amount4[index2]}}
+			labelOptions: {backgroundColor: 'rgba(255,255,255,0.5)',verticalAlign: 'top',y: 15},labels: [{point: {xAxis: 0,yAxis: 0,x: date2,y: px4[px2]},text: amount4[px2]}]}
 			} else {
 			return{
-			type: 'line', borderColor: 'red', id: 'vline' + index1, mode: 'vertical', scaleID: 'x-axis-0', value: date1, borderWidth: 1, label: {enabled: true, position: "top", content: amount[index1]}}
+			//type: 'line', borderColor: 'red', id: 'vline' + index2, mode: 'vertical', scaleID: 'x-axis-0', value: date2, borderWidth: 1, label: {enabled: true, position: "top", content: amount4[index2]}}
+			labels: [{point: {xAxis: 0,yAxis: 0,x: date2,y: px4[px2]},x: -30,text: amount4[px2]}]}
 			};
 		
-		});		
+		});				
+		
+		var elevationData = test3;
+	
+	
+
+// Now create the chart
+/*Highcharts.chart(chr, {
+
+    chart: {
+        type: 'area',
+        zoomType: 'x',
+        panning: true,
+        panKey: 'shift',
+        scrollablePlotArea: {
+            minWidth: 600
+        }
+    },
+
+    caption: {
+        text: 'This chart uses the Highcharts Annotations feature to place labels at various points of interest. The labels are responsive and will be hidden to avoid overlap on small screens.'
+    },
+
+    title: {
+        text: '2017 Tour de France Stage 8: Dole - Station des Rousses'
+    },
+
+    accessibility: {
+        description: 'Image description: An annotated line graph illustrates the 8th stage of the 2017 Tour de France cycling race from the start point in Dole to the finish line at Station des Rousses. Altitude is plotted on the Y-axis at increments of 500m and distance is plotted on the X-axis in increments of 25 kilometers. The line graph is interactive, and the user can trace the altitude level at every 100-meter point along the stage. The graph is shaded below the data line to visualize the mountainous altitudes encountered on the 187.5-kilometre stage. The three largest climbs are highlighted at Col de la Joux, Côte de Viry and the final 11.7-kilometer, 6.4% gradient climb to Montée de la Combe de Laisia Les Molunes which peaks at 1200 meters above sea level. The stage passes through the villages of Arbois, Montrond, Bonlieu, Chassal and Saint-Claude along the route.'
+    },
+
+    credits: {
+        enabled: false
+    },
+	
+	
+    annotations: test4
+	/*[
+	    {labelOptions: {
+            backgroundColor: 'rgba(255,255,255,0.5)',
+            verticalAlign: 'top',
+            y: 15
+        },
+        labels: [{
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 1583798400000,
+                y: 57.970001
+            },
+            text: 'Arbois'
+        }]}, 
+	{labels: [{
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 1613692800000,
+                y: 227.270004
+            },
+            x: -30,
+            text: 'Col de la Joux'
+        }]}
+		 ]*/,	
+
+    xAxis: {
+	    labels: {
+      format: "{value:%b %e}"
+    },
+    //tickInterval: 604800000,
+    type: "datetime"//,
+    //min: 1569888000000
+	    /*,
+      labels: {
+        formatter: function() {
+          return Highcharts.dateFormat('%b/%e/%Y', this.value);
+        }
+      }*/
+        /*labels: {
+            format: '{value}'
+        },
+        minRange: 5,
+        title: {
+            text: 'Distance'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0 to 187.8km.'
+        }*/
+    },
+
+    yAxis: {
+        startOnTick: true,
+        endOnTick: false,
+        maxPadding: 0.35,
+        title: {
+            text: null
+        },
+        labels: {
+            format: '{value}'
+        }
+    },
+
+    tooltip: {
+        formatter: function() {
+                return  '<b>' + this.series.name +'</b><br/>' +
+                    Highcharts.dateFormat('%e - %b - %Y',
+                                          new Date(this.x))
+                + ' date, ' + this.y ;
+            }//,headerFormat: 'Date: {point.x}<br>',
+        //pointFormat: '{point.y}',
+        //shared: true
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        accessibility: {
+            keyboardNavigation: {
+                enabled: false
+            }
+        },
+        data: elevationData,
+        lineColor: Highcharts.getOptions().colors[1],
+        color: Highcharts.getOptions().colors[2],
+        fillOpacity: 0.5,
+        name: 'Elevation',
+        marker: {
+            enabled: false
+        },
+        threshold: null
+    }]
+
+});*/
 		
 		var myChart=new Chart(chr, {
 		//var chart = new Chart(ctx, {
