e596e04737b62423c1909b413ec3e57cb7c1c140
and359 <79313508+and359@users.noreply.github.com>
2021-03-19 15:36:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -30,10 +30,10 @@
 }
 nav{
 	background-image: linear-gradient(to top right,red,yellow); 
-	padding-left: 300px;
+	padding-left: 100px;
 }
 .content{
-	padding-left: 300px;
+	padding-left: 100px;
 	height:100%;
 }
 .card-bg{
@@ -70,7 +70,7 @@ nav{
 	<div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo center">Trading Results: 1</a>
+			<a href="#" class="brand-logo center">Trading Results: </a>
 			<a href="" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>		
 		</div>
 	</nav>
