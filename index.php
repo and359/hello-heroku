1b0fe927b2f9d26c429aeb13a2f07a81e17514ab
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 02:25:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,10 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Collapse test 1</title>
-        <!--<link href="css/bootstrap.css" rel="stylesheet">
+        <title>Collapse test</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
         <script src="js/jquery.js"></script>
-        <script src="js/bootstrap-collapse.js"></script>-->
-	   <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet">
-	<script src="http://code.jquery.com/jquery.js"></script>
-	<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+        <script src="js/bootstrap-collapse.js"></script>
     </head>
     <body>
 
@@ -24,7 +21,3 @@
     </table>
 </body>
 </html>
-	
-	
-	
-	 		
