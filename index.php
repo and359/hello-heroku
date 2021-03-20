982ee6990876d2df119b5d2440c22922d68d0d1e
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 14:28:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> 
       
 <!--      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
-<!--       <script src="https://code.jquery.com/jquery-3.1.0.js"></script> 
+       <script src="https://code.jquery.com/jquery-3.1.0.js"></script> 
       
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!--      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
