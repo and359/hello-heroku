a0d63e7085f8b432d7a95e31229039d351ea061e
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 10:51:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-<meta charset=utf-8 />
-<title>JS Bin</title>
-</head>
 <body>
-  
-  <script>
-  $(function(){
-  
-  $(".dropdown-menu li a").click(function(){
-    
-    $(".btn:first-child").text($(this).text());
-     $(".btn:first-child").val($(this).text());
-  });
 
-});
+<h1>The option value attribute</h1>
+
+<form>
+<label for="cars">Choose a car:</label>
+
+<select id="cars" name="cars">
+  <option value="volvo">Volvo XC90</option>
+  <option value="saab">Saab 95</option>
+  <option value="mercedes">Mercedes SLK</option>
+  <option value="audi">Audi TT</option>
+</select>
+<input type="submit" value="Submit">
+</form>
+
+<p>Choose a car, and click the "Submit" button to send input to the server.</p>
+
+  <script>
+    document.getElementById("cars").value;
+    
   </script>
-  
-  <div class="btn-group">
-    <button class="btn">Please Select From List</button>
-    <button class="btn dropdown-toggle" data-toggle="dropdown">
-    <span class="caret">></span>
-   </button>
- <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
-   <li><a tabindex="-1" href="#">Item I</a></li>
-   <li><a tabindex="-1" href="#">Item II</a></li>
-   <li><a tabindex="-1" href="#">Item III</a></li>
-   <li class="divider"></li>
-   <li><a tabindex="-1" href="#">Other</a></li>
- </ul>
-</div>
 </body>
 </html>
