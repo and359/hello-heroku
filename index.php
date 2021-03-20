c8e3e1a9d54ec59568b5eee593709d1fc006f085
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 19:22:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,55 +6,13 @@
 
   <body>
     <h1>2</h1><br>
-    <form method="POST">
-      <p>Please, choose the salary id to proceed result:</p>
+    <script>
+      var p1 = "success";
+    </script>
 
-      <p>
-        <label for="salarieids">SalarieID:</label>
-                                        <?php
-                                  /* Database connection settings */
-                                  $host = 'us-cdbr-east-03.cleardb.com';
-                                  $user = 'b8a00bf633cf68';
-                                  $pass = '1a8113a0';
-                                  $db = 'heroku_69459908ed082cc';
-                                  $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
-
-                                  $data1 = '';
-                                  $data2 = '';
-                                  $date = '';
-                                  $data3 = '';
-                                  $data4 = '';
-                                  $data5 = '';
-                                  $data6 = '';
-
-                                  //query to get data from the table
-                                  $sql = "SELECT Ticker FROM heroku_69459908ed082cc.backtest GROUP BY(Ticker);";
-                                      $result = mysqli_query($mysqli, $sql);
-                                    while ($row = mysql_fetch_assoc($result)) {
-                                      echo '<option value="', $row['Ticker'], '">', $row['Ticker'], '</option>'; 
-                                      }
-                                ?>
-                                  <select id="salarieids" name="salarieid">
-                                
-                                    
-                                  
-                                  
-
-        
-          
-        </select>
-        
-      </p>
-      <p>
-        <input type="submit" value="Sumbit my choice"/>
-      </p>
-    </form>
-
-    
-        <table>
-          
-        </table>
-      
+<?php
+echo "<script>document.writeln(p1);</script>";
+?>      
 
   </body>
 </html>
