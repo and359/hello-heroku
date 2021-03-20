d44bf0b39847fdedc80a39ee44e604d0eeeb86af
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 13:53:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 	<div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo center">Trading Results: 1</a>
+			<a href="#" class="brand-logo center">Trading Results: </a>
 			<a href="" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>		
 		</div>
 	</nav>
@@ -140,7 +140,7 @@
 			</div>
 			<div class="col l12 m6 s12">
 				<div class="card card-bg">
-					<div class="card-content">
+					<!--<div class="card-content">-->
 						<h2>Form control: select</h2>
 						<div class="dropdown">
 						<select class="form-control" id="sel1">
@@ -150,7 +150,7 @@
 						</select>
 						<button onclick="myFunction1()">Try it</button>
 						</div>
-					</div>
+					<!--</div>-->
 				</div>
 			</div>
 			<p id="demo"></p>
