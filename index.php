1e8278da960fcb4b3a89da63eb4e0c4812c2349b
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 14:26:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 	<title>Trading Analytics</title>
-	<h1>1</h1>
+	<h1>2</h1>
 	
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> 
       
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<!--      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
 <!--       <script src="https://code.jquery.com/jquery-3.1.0.js"></script> 
       
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
