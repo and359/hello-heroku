45cb21bb5fb41ae0b5ee7390aa7a2cc8d0645e5c
and359 <79313508+and359@users.noreply.github.com>
2021-03-20 19:03:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -18,14 +18,10 @@
 	$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
           $query = "SELECT TICKER FROM heroku_69459908ed082cc.backtest GROUP BY(TICKER)";
           $result = mysql_query($mysqli , $query);
-          if ($result) :
+          
         ?>
         <select id="salarieids" name="salarieid">
-          <?php
-            while ($row = mysql_fetch_assoc($result)) {
-              echo '<option value="', $row['TICKER'], '">', $row['TICKER'], '</option>'; //between <option></option> tags you can output something more human-friendly (like $row['name'], if table "salaried" have one)
-            }
-          ?>
+          
         </select>
         <?php endif ?>
       </p>
@@ -34,24 +30,11 @@
       </p>
     </form>
 
-    <?php if isset($_POST['TICKER']) : ?>
-      <?php
-        $query = "SELECT * FROM `backtest` WHERE Ticker = '" . $_POST['salarieid'] . "';";
-        $result = mysql_query($mysqli , $query);
-        if ($result) :
-      ?>
+    
         <table>
-          <?php
-            while ($row = mysql_fetch_assoc($result)) {
-              echo '<tr>';
-              echo '<td>', $row['Ticker'], '</td><td>', $row['PriceDate'], '</td>'; // and others
-              echo '</tr>';
-            }
-          ?>
+          
         </table>
-      <?php endif?>
-    <?php endif ?>
-
+      
 
   </body>
 </html>
