ec281fb2d9df8c07be15dcfb5d9eb372d3e62dc9
and359 <79313508+and359@users.noreply.github.com>
2022-04-20 10:24:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title>Trading Analytic</title>
+	<title>Trading </title>
        
 	<!--Import materialize.css-->
       <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
