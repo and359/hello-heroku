4bc887aa7aaca4a76e005c1e0020230d481e758e
and359 <79313508+and359@users.noreply.github.com>
2022-04-20 10:13:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>Trading Analytics</title>
        
-	 	
-	<!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Let browser know website is optimized for mobile-->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 	<!--Import materialize.css-->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link rel = "stylesheet"
